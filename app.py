da5b4bc1fb4e8f4bbd1a67017f803d93d948ee91
Vishalvirdi <vishalvirdi039@gmail.com>
2024-04-04 15:32:37 +0530

    res done

--- app.py
+++ app.py
@@ -7,46 +7,41 @@ app = Flask(__name__)
 
 min_colony_area = 100
 
-    
-@app.route('/', methods=['GET'])
-def get():
-    return jsonify({"message": "Hello from the root!"})
+@app.route('/', methods=['POST', 'GET'])
+def analyze_image():
 
-# @app.route('/', methods=['POST'])
-# def analyze_image():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file provided'}), 400
 
-#     if 'image' not in request.files:
-#         return jsonify({'error': 'No image file provided'}), 400
-
-#     uploaded_image = request.files['image']
+    uploaded_image = request.files['image']
     
 
-#     allowed_extensions = {'jpg', 'jpeg', 'png'}
-#     if uploaded_image.filename.split('.')[-1].lower() not in allowed_extensions:
-#         return jsonify({'error': 'Invalid file extension. Only JPG, JPEG, and PNG are supported'}), 400
+    allowed_extensions = {'jpg', 'jpeg', 'png'}
+    if uploaded_image.filename.split('.')[-1].lower() not in allowed_extensions:
+        return jsonify({'error': 'Invalid file extension. Only JPG, JPEG, and PNG are supported'}), 400
     
-#     image_data = uploaded_image.read()
+    image_data = uploaded_image.read()
     
-#     nparr = np.fromstring(image_data, np.uint8)
+    nparr = np.fromstring(image_data, np.uint8)
     
-#     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     
-#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-#     edges = cv2.Canny(blurred, threshold1=30, threshold2=150)
+    edges = cv2.Canny(blurred, threshold1=30, threshold2=150)
 
-#     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-#     colony_count = 0
+    colony_count = 0
 
-#     for contour in contours:
-#         area = cv2.contourArea(contour)
-#         if area > min_colony_area:
-#             colony_count += 1
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > min_colony_area:
+            colony_count += 1
     
-#     return jsonify({'colony_count': colony_count})
+    return jsonify({'colony_count': colony_count})
     
     
     
